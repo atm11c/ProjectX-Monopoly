80bf1673634aaf9cbe10106078a1ddbf1735be1e
Timothy <timothy.barao@gmail.com>
2015-11-21 15:43:56 -0500

    Board near done, ned to add card slots in center of board, may change from buttons to another container

--- src/Menu.java
+++ src/Menu.java
@@ -14,9 +14,16 @@ public class Menu {
     public static void main(String[] args){
         JFrame frame = new JFrame("Monopoly!");
         MenuPanel mPanel = new MenuPanel();
+
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth()/2 - frame.getWidth())/2);
+        int y = (int) ((dimension.getHeight()/2 - frame.getHeight()) / 2);
+
+
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         frame.setSize( 600, 400 );
         frame.setVisible( true );
+        frame.setLocation(x, y);
         frame.add(mPanel);
     }
 
@@ -93,7 +100,8 @@ class MenuPanel extends JPanel{
             if(event.getSource() == start){
                 JFrame gFrame = new JFrame("Game Board");
                 BoardPanel test1 = new BoardPanel();
-                gFrame.setSize( 1000, 1000 );
+
+                gFrame.setSize( 1100, 1100 );
                 gFrame.setResizable(false);
                 gFrame.setVisible( true );
                 gFrame.add(test1);
@@ -123,53 +131,83 @@ class BoardPanel extends JPanel
         setLayout(new BorderLayout());
         JPanel t1, t2, t3, t4;
 
-        JButton dumb[] = new JButton[40];
+        JButton dumb[] = new JButton[44];
         //JLabel dumb[] =  new JLabel[40];
 
         ImageIcon man = new ImageIcon( this.getClass().getResource("Monopoly-artwork-detail.jpg"));
         JLabel pic = new JLabel(man);
 
-        for(int i = 0; i < 40; i++){
-            dumb[i] = new JButton("Diddle");
-            //dumb[i] = new JLabel("test");
+        for(int i = 0; i < 44; i++){
+
+
+            if(i == 2 || i == 4  || i == 7  || i == 9 || i == 10  || i == 12  || i == 14 || i == 15
+                      || i == 17 || i == 19 || i == 20 || i == 22 || i == 24 || i == 25 || i == 27
+                      || i == 28 || i == 30 || i == 32 || i == 33 || i == 35 || i == 38 || i == 40) {
+                dumb[i] = new JButton("Property");
+            }else if(i == 6 || i == 16 || i == 26 || i == 36){
+                dumb[i] = new JButton("Train");
+            }else if(i == 3 || i == 18 || i == 34) {
+                dumb[i] = new JButton("Chest");
+            }else if(i == 8 || i == 23 || i == 37) {
+                dumb[i] = new JButton("Chance");
+            }else if(i == 5){
+                dumb[i] = new JButton("Income Tax");
+            }else if(i == 1) {
+                dumb[i] = new JButton("Go");
+            }else if(i == 11){
+                dumb[i] = new JButton("Jail");
+            }else if(i == 13){
+                dumb[i] = new JButton("Electric");
+            }else if(i == 21){
+                dumb[i] = new JButton("Parking");
+            }else if(i == 29){
+                dumb[i] = new JButton("Water");
+            }else if(i == 31){
+                dumb[i] = new JButton("Go to Jail");
+            }else if(i == 39){
+                dumb[i] = new JButton("Lux Tax");
+            }else {
+                dumb[i] = new JButton("");
+            }
+
             dumb[i].setSize(100,100);
-            //dumb[i].setIcon(man);
         }
 
         t1 = new JPanel();
         t2 = new JPanel();
+
         t3 = new JPanel();
         t4 = new JPanel();
 
-        t1.setLayout(new GridLayout(1,10));
-        t1.setPreferredSize(new Dimension(1000,100));
-        t2.setLayout(new GridLayout(1,10));
-        t2.setPreferredSize(new Dimension(1000,100));
-        t3.setLayout(new GridLayout(8,1));
-        t3.setPreferredSize(new Dimension(100,800));
-        t4.setLayout(new GridLayout(8,1));
-        t4.setPreferredSize(new Dimension(100,800));
+        t1.setLayout(new GridLayout(1,11));
+        t1.setPreferredSize(new Dimension(1100,100));
+        t2.setLayout(new GridLayout(1,11));
+        t2.setPreferredSize(new Dimension(1100,100));
 
-        int counter = 0;
-        for(int i = 0; i < 10; i++){
+        t3.setLayout(new GridLayout(9,1));
+        t3.setPreferredSize(new Dimension(100,900));
+        t4.setLayout(new GridLayout(9,1));
+        t4.setPreferredSize(new Dimension(100,900));
 
-            t1.add(dumb[counter]);
-            counter++;
-            t2.add(dumb[counter]);
-            counter++;
+        for(int i = 11; i > 0; i--){
+            t1.add(dumb[i]);
+        }
+        for(int i = 21; i >11; i--){
+            t3.add(dumb[i]);
+        }
+        for(int i = 21; i < 32; i++){
+            t2.add(dumb[i]);
+        }
+        for(int i = 32; i < 41; i++){
+            t4.add(dumb[i]);
         }
 
-        for(int i = 0; i < 8;i++){
-            t3.add(dumb[counter]);
-            counter++;
-            t4.add(dumb[counter]);
-            counter++;
-        }
+        add(t1, BorderLayout.SOUTH);
+        add(t2, BorderLayout.NORTH);
 
-        add(t1, BorderLayout.NORTH);
-        add(t2, BorderLayout.SOUTH);
-        add(t3, BorderLayout.EAST);
-        add(t4, BorderLayout.WEST);
+        add(t3, BorderLayout.WEST);
+        add(t4, BorderLayout.EAST);
+
         add(pic, BorderLayout.CENTER);
 
     }
