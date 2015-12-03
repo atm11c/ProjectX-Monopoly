e927901dceac89bd02b2c1fd549036ce763e730a
atm11c <atm11c@my.fsu.edu>
2015-12-03 10:25:18 -0500

    Reapplied checking chance and community chests

--- src/Player.java
+++ src/Player.java
@@ -829,5 +829,32 @@ public class Player {
             return p3;
     }
 
+    public void checkChanceCard(int x){
+        switch(x){
+
+        }
+    }
+
+    public void checkChestCard(int y){
+        switch (y){
+            case 0:
+                System.out.println("Collect $50");
+                    break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+        }
+    }
 
 }
